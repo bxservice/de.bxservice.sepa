9fce5deb911514103b77391e96f5d3c21732c6cc
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2018-01-25 19:12:59 +0100

    SDD and SCT SEPA files

---
+++ src/de/bxservice/sepa/MBPBankAccountHelper.java
@@ -0,0 +1,26 @@
+package de.bxservice.sepa;
+
+import org.compiere.model.MBPBankAccount;
+
+/**
+ * I extended the table of {@link MBPBankAccount} but I did not want to exchange
+ * the whole model classes. Thats why here are the things I added to this Model
+ * for my own purposes.
+ * 
+ * @author tbayen
+ */
+public class MBPBankAccountHelper {
+
+	/**
+	 * german "Lastschriftart", has one of the scheme names as string.
+	 */
+	public static final String COLUMNNAME_SEPASDDSCHEME="SepaSddScheme";
+	public static final String COLUMNNAME_ISTRANSFERRED="IsTransferred";
+	public static final String COLUMNNAME_MNDTID="MndtId";
+	public static final String COLUMNNAME_DATEDOC="DateDoc";
+	public static final String COLUMNNAME_IBAN="IBAN";
+	
+	static public enum SepaSddScheme {
+		CORE, COR1, B2B
+	};
+}
---
+++ src/de/bxservice/sepa/MOrgHelper.java
@@ -0,0 +1,10 @@
+package de.bxservice.sepa;
+
+/**
+ * This class hosts extensions I did to the iDempiere MOrg class.
+ * 
+ * @author tbayen
+ */
+public class MOrgHelper {
+	static public final String COLUMNNAME_AD_ORG_CREDITORIDENTIFIER = "CreditorIdentifier";
+}
